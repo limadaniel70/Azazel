57b93407c9de808e02e8e318fa308f712a7e441c
Daniel Lima <dnlima77237@gmail.com>
2024-09-20 19:11:07 -0300

    Fixed format

--- azazel/bot.py
+++ azazel/bot.py
@@ -1,8 +1,23 @@
+#    Copyright 2024 Daniel Lima
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
 import logging
-import discord
-from discord.ext import commands
-import dotenv
 import os
+
+import discord
+import dotenv
+from discord.ext import commands
 
 dotenv.load_dotenv()
 
@@ -15,16 +30,18 @@ formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
 
 enviroment = os.getenv("ENVIROMENT")
 if enviroment == "DEVELOPMENT":
-    level = 10 # DEBUG
+    level = 10  # DEBUG
 else:
-    level = 20 # INFO
+    level = 20  # INFO
 
 intents = discord.Intents().default()
 
 bot = commands.Bot(command_prefix="zl", intents=intents)
 
+
 @bot.event
 async def on_ready():
     print(f"Logged as {bot.user}")
+
 
 bot.run(token=token)
