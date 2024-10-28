43ea443824fcf8eee7766348a15d4fa0c9360320
Daniel Lima <dnlima77237@gmail.com>
2024-10-28 06:04:54 -0300

    Rewrote bot

--- azazel/azazel_bot.py
+++ azazel/azazel_bot.py
@@ -1,21 +1,19 @@
-#    Copyright 2024 Daniel Lima
+#     Copyright 2024 Daniel Lima
 #
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
 #
-#        http://www.apache.org/licenses/LICENSE-2.0
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
-
-import asyncio
-import logging
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
 import os
-# import pathlib
+from math import expm1
+from pathlib import Path
 
 import discord
 import dotenv
@@ -25,47 +23,25 @@ from azazel.utils.exceptions import NullToken
 
 dotenv.load_dotenv()
 
-token = os.getenv("DISCORD_API_KEY")
-if token is None:
+TOKEN = os.getenv("DISCORD_API_KEY")
+if TOKEN is None:
     raise NullToken("The token cannot be empty!")
 
-enviroment = os.getenv("ENVIROMENT")
-if enviroment == "DEVELOPMENT":
-    level = 10  # DEBUG
-else:
-    level = 20  # INFO
 
-file_handler = logging.FileHandler(filename="azazel.log", encoding="utf-8", mode="a")
-logging.basicConfig(
-    handlers=[file_handler], format="%(asctime)s %(levelname)s %(message)s", level=level
-)
-logger = logging.getLogger(__name__)
+class AzazelBot(commands.Bot):
+    def __init__(self):
+        super().__init__("!!", intents=discord.Intents.all())
 
-intents = discord.Intents().all()
-
-bot = commands.Bot(command_prefix="zl", intents=intents)
+    async def setup_hook(self) -> None:
+        for cog in Path("./azazel/cogs").iterdir():
+            if Path(cog).suffix == ".py":
+                try:
+                    await self.load_extension(f"cogs.{str(cog)}")
+                    print(f"Cog loaded successfully: {str(cog)}")
+                except Exception as e:
+                    print(f"[*] Cog failed: {str(cog)}\nError: {e}")
 
 
-@bot.event
-async def on_ready() -> None:
-    await bot.tree.sync()
-    logger.info("Bot ready as %s", bot.user)
-
-
-# async def load() -> None:
-#     """
-#     Loads the commands.
-#     """
-#     for file in pathlib.Path("azazel/commands").iterdir():
-#         if file.suffix == ".py":
-#             await bot.load_extension(f"commands.{file.name[:-3]}")
-
-# @bot.tree.command(name="hello",description="Says hello world")
-# async def slash_command(interaction:discord.Interaction):
-#     await interaction.response.send_message("Hello World!")
-
-async def main() -> None:
-    # await load()
-    await bot.start(str(token))
-
-asyncio.run(main())
+if __name__ == "__main__":
+    client = AzazelBot()
+    client.run(TOKEN)
