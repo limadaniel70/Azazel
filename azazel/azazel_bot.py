c847cd7a28f098f7f6693576a66d698d42e768a9
Daniel Lima <dnlima77237@gmail.com>
2024-10-28 06:24:34 -0300

    Fixed imports

--- azazel/azazel_bot.py
+++ azazel/azazel_bot.py
@@ -1,18 +1,17 @@
-#     Copyright 2024 Daniel Lima
+#  Copyright 2024 Daniel Lima
 #
-#     Licensed under the Apache License, Version 2.0 (the "License");
-#     you may not use this file except in compliance with the License.
-#     You may obtain a copy of the License at
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
 #
-#         http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
-#     Unless required by applicable law or agreed to in writing, software
-#     distributed under the License is distributed on an "AS IS" BASIS,
-#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#     See the License for the specific language governing permissions and
-#     limitations under the License.
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 import os
-from math import expm1
 from pathlib import Path
 
 import discord
@@ -33,11 +32,11 @@ class AzazelBot(commands.Bot):
         super().__init__("!!", intents=discord.Intents.all())
 
     async def setup_hook(self) -> None:
-        for cog in Path("./azazel/cogs").iterdir():
+        for cog in Path("cogs").iterdir():
             if Path(cog).suffix == ".py":
                 try:
-                    await self.load_extension(f"cogs.{str(cog)}")
-                    print(f"Cog loaded successfully: {str(cog)}")
+                    await self.load_extension(f"cogs.{str(cog.parts[-1][:-3])}")
+                    print(f"Cog loaded successfully: {str(cog.parts[-1])}")
                 except Exception as e:
                     print(f"[*] Cog failed: {str(cog)}\nError: {e}")
 
