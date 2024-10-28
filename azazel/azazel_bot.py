b378049224a6d7741bb72779ab26fa80eda0a114
Daniel Lima <dnlima77237@gmail.com>
2024-10-28 07:04:34 -0300

    Added logging

--- azazel/azazel_bot.py
+++ azazel/azazel_bot.py
@@ -11,6 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+import logging
 import os
 from pathlib import Path
 
@@ -26,21 +27,43 @@ TOKEN = os.getenv("DISCORD_API_KEY")
 if TOKEN is None:
     raise NullToken("The token cannot be empty!")
 
+logger = logging.getLogger("discord")
+formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] %(name)s: %(message)s")
+handler = logging.StreamHandler()
+
+enviroment = os.getenv("ENVIROMENT")
+if enviroment == "DEVELOPMENT":
+    logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.INFO)
+
+
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 
 class AzazelBot(commands.Bot):
     def __init__(self):
         super().__init__("!!", intents=discord.Intents.all())
+        logger.info("Starting AzazelBot")
+
+    async def on_ready(self):
+        logger.info("Logged in")
 
     async def setup_hook(self) -> None:
-        for cog in Path("cogs").iterdir():
+        for cog in Path("./azazel/cogs").iterdir():
             if Path(cog).suffix == ".py":
                 try:
+                    # example cog: PosixPath("cogs/ping.py")
+                    # str(cog.parts[-1][:-3]) -> "ping"
                     await self.load_extension(f"cogs.{str(cog.parts[-1][:-3])}")
-                    print(f"Cog loaded successfully: {str(cog.parts[-1])}")
+                    logger.info("Cog loaded successfully: %s", str(cog.parts[-1]))
+                except FileNotFoundError:
+                    logger.error("couldn't find %s", str(cog))
                 except Exception as e:
-                    print(f"[*] Cog failed: {str(cog)}\nError: {e}")
+                    logger.error("Cog failed: %s\nError: %s", str(cog), e)
 
 
 if __name__ == "__main__":
     client = AzazelBot()
-    client.run(TOKEN)
+    client.run(TOKEN, log_handler=None)
