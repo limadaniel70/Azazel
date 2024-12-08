19289a2e744b931834b293a02af37294c9460615
Daniel Lima <dnlima77237@gmail.com>
2024-12-08 20:55:48 -0300

    Add command not found error

--- azazel/cogs/errors/error_handler.py
+++ azazel/cogs/errors/error_handler.py
@@ -15,8 +15,10 @@ import logging
 from discord.ext.commands import Bot, Cog, Context, CommandError
 from discord.ext.commands import (
     MemberNotFound,
+    UserNotFound,
     MissingPermissions,
     MissingRequiredArgument,
+    CommandNotFound,
 )
 
 
@@ -29,7 +31,7 @@ class ErrorHandler(Cog):
 
     @Cog.listener()
     async def on_command_error(self, ctx: Context[Bot], error: CommandError) -> None:
-        if isinstance(error, MemberNotFound):
+        if isinstance(error, MemberNotFound) or isinstance(error, UserNotFound):
             await ctx.send(f"Could not find member '{error.argument}'.")
         elif isinstance(error, MissingPermissions):
             await ctx.send(
@@ -38,9 +40,12 @@ class ErrorHandler(Cog):
             )
         elif isinstance(error, MissingRequiredArgument):
             await ctx.send(f"'{error.param.name}' is a required argument.")
+        elif isinstance(error, CommandNotFound):
+            command_name = str(error).split('"')[1]
+            await ctx.send(f"There's no command named '{command_name}'.")
         else:
             logger.error("Unhandled error: %s", error)
-            await ctx.send("An unexpected error ocurred. Please try again later.")
+            await ctx.send("An unexpected error ocurred.")
 
     # @Cog.listener()
     # async def on_error(self) -> None:
